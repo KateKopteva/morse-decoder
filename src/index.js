08743379dcd1b3c90c5e8d58df74ae676fea0639
OASU <katya_bychko@mail.ru>
2023-08-24 12:05:24 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -35,11 +35,37 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+
 };
 
 function decode(expr) {
-    // write your solution here
+    let arr = [];
+    let letter = [];
+    let word;
+    let breakExpr = expr.match(/.{10}/g)
+    for(let i = 0; i < breakExpr.length; i++) {
+        breakExpr[i] = breakExpr[i].replace('**********',' ');
+        };
+    for (let item of breakExpr) {
+        morseCodeArr = arr.push(item.replace(/00/g, '').replace(/10/g, '.').replace(/11/g, '-')); 
+                                                                //['....', '.', '.-..', '.-..', '---', ' ', '.--', '---', '.-.', '.-..', '-..']
+    }
+    let index = arr.length;
+    for (let i = 0; index-- > 0; i++){
+        for (let key in MORSE_TABLE) {
+            if (MORSE_TABLE.hasOwnProperty(key) && arr[i] === key) { 
+                letter[i] = MORSE_TABLE[arr[i]];
+                letter.push(word);
+            }                                                   //['h', 'e', 'l', 'l', 'o', undefined, 'w', 'o', 'r', 'l', 'd', undefined]
+            if (letter[i] === undefined) {
+                letter[i] = arr[i];
+            }
+        }
+    }
+    let result = letter.join('');
+    return result;
 }
+
 
 module.exports = {
     decode
